a9fd50b28fc4a3c1840fc728b09d6be76656cdef
bkirsh99 <bkirsh.3006@gmail.com>
2021-04-08 23:16:21 -0700

    Added OPTION #3 to compute hypergeometric tests.

--- test_input2.R
+++ test_input2.R
@@ -31,7 +31,6 @@ ALL.df$`GWAS P-value` <- data$`GWAS P-value`
 ALL.df[is.na(ALL.df)] <- 0
 
 dfs <- paste(populations,"df",sep=".")
-hyper.df <- data.frame()
 
 for(i in seq(from=4, to=ncol(data), by=5)){
   population <- toString(data[1,i])
@@ -45,6 +44,18 @@ for(i in seq(from=4, to=ncol(data), by=5)){
   }
 }
 
+#Calculating the hypergeometric distributions using phyper().
+#A random variable X has a hypergeometric distribution if X=x is the number of successes in a sample size of size k (without replacement) when the population contains M successes and N non-successes.
+#For example, the probability of selecting 14 red marbles from a sample of 20 taken from an urn containing 70 red marbles and 30 green marbles is phyper(x=14,m=70,n=30,k=20).
+#Thus, for each SNP, the parameters for this function are:
+#q: number of effect alleles selected from the sample
+#m: total number of effect alleles in the population
+#n: total number of other alleles in the population
+#k:	total number of alleles (effect + other) in the sample
+#lower.tail=TRUE (depletion) or FALSE (enrichment)
+
+#OPTION 1:
+hyper.df <- data.frame()
 for(df in dfs){
   a <- c()
   info <- get(df)
@@ -58,6 +69,8 @@ for(df in dfs){
   hyper.df[df] <- a
 }
 
+#OPTION 2:
+hyper.df <- data.frame()
 for(i in 1:nrow(ALL.df)){
   a <- c()
   for (df in dfs){
@@ -82,6 +95,48 @@ pvalFiltered.df <- subset(hyper.df < cutoff)
 enriched <- data.frame(hyper.df < cutoff)
 table(rowSums(enriched))
 v <- ggvenn(enriched)
+
+#OPTION 3:
+enrichment.df <- data.frame()
+depletion.df <- data.frame()
+for(i in 1:nrow(ALL.df)){
+  a <- c()
+  b <- c()
+  for (df in dfs){
+    info <- get(df)
+    m <- as.numeric(ALL.df$`effect allele number`[i]) #effect alleles in population
+    n <- as.numeric(ALL.df$`other allele number`[i]) #other alleles in population
+    k <- as.numeric(info$`total allele number`[i]) #total alleles in sample
+    q <- as.numeric(info$`effect allele number`[i]) #effect alleles in sample
+    a <- cbind(a,phyper(q=q-1,m=m,n=n,k=k,lower.tail=FALSE)) #When we set parameter lower.tail=FALSE in phyper the interpretation of the p-value is P[X > x] . But what we need to test is the null hypothesis P[X ≥ x], so we subtract x by 1.
+    b <- cbind(b,phyper(q=q,m=m,n=n,k=k,lower.tail=TRUE))
+  }
+  enrichment.df <- rbind(enrichment.df,a)
+  depletion.df <- rbind(depletion.df,b)
+}
+
+L <- c("enrichment.df","depletion.df")
+#Assign new column and row names to each dataframe in "L"
+for(df in L) {
+  df.tmp <- get(df)
+  colnames(df.tmp) <- populations
+  rownames(df.tmp) <- SNP.df$`SNP ID`
+  assign(df, df.tmp)
+}
+
+cutoff <- 0.01 / (2*3*225) #To control a family-wise error rate (FWER) of 0.01, we used a raw p-value of 7.41E-6 as cutoff. 
+enrichedFiltered.df <- subset(enrichment.df < cutoff)
+depleatedFiltered.df <- subset(depletion.df < cutoff)
+
+enriched <- data.frame(enrichment.df < cutoff)
+depleated <- data.frame(depletion.df < cutoff)
+
+table(rowSums(enriched))
+v1 <- ggvenn(enriched)
+v1
+table(rowSums(depleated))
+v2 <- ggvenn(depleated)
+v2
 
 
 MAF.df["id"] <- rownames(MAF.df)
